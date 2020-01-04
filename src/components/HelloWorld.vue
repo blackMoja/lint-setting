ef1f94f17512368e5efc55018f42bc68d15500fb
blackMoja <limlim980625@gmail.com>
2020-01-04 23:29:47 +0900

    -테스트 커밋

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -69,35 +69,35 @@
 </template>
 
 <script>
-export default {
-	name: 'HelloWorld',
+	export default {
+		name: 'someThing',
 
-	props: {
-		msg: String
-	}
-};
+		props: {
+			msg: String
+		}
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
-h3 {
-	margin: 40px 0 0;
-}
+	h3 {
+		margin: 40px 0 0;
+	}
 
-ul {
-	list-style-type: none;
+	ul {
+		list-style-type: none;
 
-	padding: 0;
-}
+		padding: 0;
+	}
 
-li {
-	display: inline-block;
+	li {
+		display: inline-block;
 
-	margin: 0 10px;
-}
+		margin: 0 10px;
+	}
 
-a {
-	color: #42b983;
-}
+	a {
+		color: #42b983;
+	}
 </style>
